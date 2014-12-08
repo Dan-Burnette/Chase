f5cc47274aef09633820c248d9b0928201d958df
Daniel Burnette <dcburnette999@gmail.com>
2014-12-08 14:13:02 -0600

    Got a div moving around randomly

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response) {
-	response.send('Hi');
-});
 
-
-
+app.use(express.static(__dirname + '/public'));
 
 
 
